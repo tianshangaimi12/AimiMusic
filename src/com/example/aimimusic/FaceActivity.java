4a12e811834d8597efc7e75073c4f9a2854e5009
zhangchong <731411677@qq.com>
2018-01-15 16:41:59 +0800

    完善启动界面

--- src/com/example/aimimusic/FaceActivity.java
+++ src/com/example/aimimusic/FaceActivity.java
@@ -3,55 +3,68 @@ package com.example.aimimusic;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.aimimusic.utils.HttpUtils;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class FaceActivity extends Activity{
-	private TextView mTextView;
-	private int index;
-	private String title;
-	private char[] array;
-	private StringBuffer sb = new StringBuffer();
+	private ImageView mImageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_face);
-		mTextView = (TextView)findViewById(R.id.txt_face_title);
-		title = getResources().getString(R.string.face_activity_title);
-		array = title.toCharArray();
-		index = 0;
-		new Timer().schedule(new TimerTask() {
-			
-			@Override
-			public void run() {
-				sb.append(array[index]);
-				mTextView.post(new Runnable() {
-					
-					@Override
-					public void run() {
-						mTextView.setText(sb.toString());
-					}
-				});
-				index++;
-				if(index>=array.length)
-				{
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					this.cancel();
-					Intent intent = new Intent(FaceActivity.this,MainActivity.class);
-					startActivity(intent);
-					FaceActivity.this.finish();
-				}
-			}
-		}, 100,300);
+		mImageView = (ImageView)findViewById(R.id.img_bing_pic);
+		getBingPic();
 	}
 	
+	public void getBingPic()
+	{
+		StringRequest request = new StringRequest(HttpUtils.BING_PIC_URL, 
+				new Response.Listener<String>() {
+
+					@Override
+					public void onResponse(String response) {
+						Picasso.with(FaceActivity.this)
+						.load(response)
+						.into(mImageView,new Callback() {
+							
+							@Override
+							public void onSuccess() {
+								mImageView.postDelayed(new Runnable() {
+									
+									@Override
+									public void run() {
+										Intent intent = new Intent(FaceActivity.this, MainActivity.class);
+										startActivity(intent);
+										FaceActivity.this.finish();
+									}
+								}, 2000);
+							}
+							
+							@Override
+							public void onError() {
+							}
+						});
+					}
+				}, 
+				new Response.ErrorListener() {
+
+					@Override
+					public void onErrorResponse(VolleyError error) {
+					}
+				});
+		MusicApplication.getRequestQueue(this).add(request);
+	}
 }
--- src/com/example/aimimusic/utils/HttpUtils.java
+++ src/com/example/aimimusic/utils/HttpUtils.java
@@ -2,6 +2,7 @@ package com.example.aimimusic.utils;
 
 public class HttpUtils {
 	
+	public static final String BING_PIC_URL = "http://guolin.tech/api/bing_pic";
 	public static final String BASE_URL = "http://tingapi.ting.baidu.com/v1/restserver/ting?format=json&calback=&from=webapp_music&method=";
 	
 	/**
